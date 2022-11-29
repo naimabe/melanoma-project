de48e3cda5ee4631f3863c659a74b874583d8941
George Sims <george-sims@outlook.com>
2022-11-29 13:41:01 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+# setup.py
+from setuptools import setup
+
+setup(name='melanoma-prediction',
+        description="package description",
+        packages=["melanoma-prediction"]) # You can have several packages, try it
