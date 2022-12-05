5e6280624d0d5673da32eebcb292456d8fdbc8a1
dejantodorovic <dejan.todorovic79@gmail.com>
2022-12-05 09:36:50 +0100

    Premier semain

--- src/ml_logic/model.py
+++ src/ml_logic/model.py
@@ -1,5 +1,9 @@
 from keras.applications import EfficientNetB7
 from keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential
+from src.ml_logic.preproc import get_X_y
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import models, layers
 from keras import layers
 import tensorflow as tf
 
@@ -31,9 +35,19 @@ def initialise_EfficientNet_model():
 #     model = Sequential()
 
 def initialize_tabulaire_model():
-    model = models.Sequential()
+    model = Sequential()
     model.add(layers.Dense(9, activation='softmax', input_dim=11))
     model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
     return model
+
+def train_model(model):
+    X, y = get_X_y()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
+    history = model.fit(X_train, y_train,
+              batch_size=32,
+              epochs=10,
+              validation_split=0.3,
+              verbose=0)
+    return model,X_test,y_test
